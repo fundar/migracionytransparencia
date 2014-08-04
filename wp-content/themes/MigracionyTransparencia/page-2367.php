40bf626e34698950812ab5e8fecc00d8a2aef096
CeciliaMadrid <lya.madrid@gmail.com>
2014-08-04 18:11:36 -0500

    home

--- wp-content/themes/MigracionyTransparencia/page-2367.php
+++ wp-content/themes/MigracionyTransparencia/page-2367.php
@@ -67,8 +67,61 @@ get_header(); ?>
 					<?php endif; ?>
 				</ul>
 			</div>
-			<?php endif; ?>			
-
+			<?php endif; ?>
+			<?php else: ?>
+			<?php
+			if((has_post_thumbnail() || get_post_meta($post->ID, 'pyre_video', true))):
+			?>
+			<div class="flexslider post-slideshow">
+				<ul class="slides">
+					<?php if(!$smof_data['posts_slideshow']): ?>
+					<?php if(get_post_meta($post->ID, 'pyre_video', true)): ?>
+					<li>
+						<div class="full-video">
+							<?php echo get_post_meta($post->ID, 'pyre_video', true); ?>
+						</div>
+					</li>
+					<?php elseif(has_post_thumbnail() ): ?>
+					<?php $attachment_image = wp_get_attachment_image_src(get_post_thumbnail_id(), 'full'); ?>
+					<?php $full_image = wp_get_attachment_image_src(get_post_thumbnail_id(), 'full'); ?>
+					<?php $attachment_data = wp_get_attachment_metadata(get_post_thumbnail_id()); ?>
+					<li>
+						<a href="<?php echo $full_image[0]; ?>" rel="prettyPhoto[gallery<?php the_ID(); ?>]" title="<?php echo get_post_field('post_excerpt', get_post_thumbnail_id()); ?>"><img src="<?php echo $attachment_image[0]; ?>" alt="<?php echo get_post_meta(get_post_thumbnail_id(), '_wp_attachment_image_alt', true); ?>" /></a>
+					</li>
+					<?php endif; ?>
+					<?php else: ?>
+					<?php if(get_post_meta($post->ID, 'pyre_video', true)): ?>
+					<li>
+						<div class="full-video">
+							<?php echo get_post_meta($post->ID, 'pyre_video', true); ?>
+						</div>
+					</li>
+					<?php endif; ?>
+					<?php if(has_post_thumbnail() ): ?>
+					<?php $attachment_image = wp_get_attachment_image_src(get_post_thumbnail_id(), 'full'); ?>
+					<?php $full_image = wp_get_attachment_image_src(get_post_thumbnail_id(), 'full'); ?>
+					<?php $attachment_data = wp_get_attachment_metadata(get_post_thumbnail_id()); ?>
+					<li>
+						<a href="<?php echo $full_image[0]; ?>" rel="prettyPhoto[gallery<?php the_ID(); ?>]" title="<?php echo get_post_field('post_excerpt', get_post_thumbnail_id()); ?>"><img src="<?php echo $attachment_image[0]; ?>" alt="<?php echo get_post_meta(get_post_thumbnail_id(), '_wp_attachment_image_alt', true); ?>" /></a>
+					</li>
+					<?php endif; ?>
+					<?php
+					$i = 2;
+					while($i <= $smof_data['posts_slideshow_number']):
+					$attachment_new_id = kd_mfi_get_featured_image_id('featured-image-'.$i, 'page');
+					if($attachment_new_id):
+					?>
+					<?php $attachment_image = wp_get_attachment_image_src($attachment_new_id, 'full'); ?>
+					<?php $full_image = wp_get_attachment_image_src($attachment_new_id, 'full'); ?>
+					<?php $attachment_data = wp_get_attachment_metadata($attachment_new_id); ?>
+					<li>
+						<a href="<?php echo $full_image[0]; ?>" rel="prettyPhoto[gallery<?php the_ID(); ?>]" title="<?php echo get_post_field('post_excerpt', $attachment_new_id); ?>"><img src="<?php echo $attachment_image[0]; ?>" alt="<?php echo get_post_meta($attachment_new_id, '_wp_attachment_image_alt', true); ?>" /></a>
+					</li>
+					<?php endif; $i++; endwhile; ?>
+					<?php endif; ?>
+				</ul>
+			</div>
+			<?php endif; ?>
 			<?php endif; ?>
 			<?php endif; ?>
 			<?php endif; // password check ?>
@@ -76,7 +129,8 @@ get_header(); ?>
 				<?php the_content(); ?>
 				<?php wp_link_pages(); ?>
 			</div>
-
+			<?php if( ! post_password_required($post->ID) ): ?>
+			<?php if($smof_data['comments_pages']): ?>
 				<?php
 				wp_reset_query();
 				comments_template();
