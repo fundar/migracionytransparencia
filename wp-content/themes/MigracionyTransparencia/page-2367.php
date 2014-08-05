9961dd48672a632aed17aa804d5bf9d201889ac3
CeciliaMadrid <lya.madrid@gmail.com>
2014-08-05 00:09:05 -0500

    default pege for home

--- wp-content/themes/MigracionyTransparencia/page-2367.php
+++ wp-content/themes/MigracionyTransparencia/page-2367.php
@@ -1,12 +1,44 @@
-<?php
-// Template Name: Full Width
-get_header(); ?>
-	<div id="content" class="full-width">
-		<?php while(have_posts()): the_post(); ?>
+<?php get_header(); ?>
+	<?php
+	$content_css = '';
+	$sidebar_css = '';
+	$sidebar_exists = true;
+	if(get_post_meta($post->ID, 'pyre_full_width', true) == 'yes') {
+		$content_css = 'width:100%';
+		$sidebar_css = 'display:none';
+		$sidebar_exists = false;
+	}
+	elseif(get_post_meta($post->ID, 'pyre_sidebar_position', true) == 'left') {
+		$content_css = 'float:right;';
+		$sidebar_css = 'float:left;';
+	} elseif(get_post_meta($post->ID, 'pyre_sidebar_position', true) == 'right') {
+		$content_css = 'float:left;';
+		$sidebar_css = 'float:right;';
+	} elseif(get_post_meta($post->ID, 'pyre_sidebar_position', true) == 'default') {
+		if($smof_data['default_sidebar_pos'] == 'Left') {
+			$content_css = 'float:right;';
+			$sidebar_css = 'float:left;';
+		} elseif($smof_data['default_sidebar_pos'] == 'Right') {
+			$content_css = 'float:left;';
+			$sidebar_css = 'float:right;';
+		}
+	}
+	if(class_exists('Woocommerce')) {
+		if(is_cart() || is_checkout() || is_account_page() || (get_option('woocommerce_thanks_page_id') && is_page(get_option('woocommerce_thanks_page_id')))) {
+			$content_css = 'width:100%';
+			$sidebar_css = 'display:none';
+			$sidebar_exists = false;
+		}
+	}
+	?>
+	<div id="content" style="<?php echo $content_css; ?>">
+		<?php
+		if(have_posts()): the_post();
+		?>
 		<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 			<span class="entry-title" style="display: none;"><?php the_title(); ?></span>
 			<span class="vcard" style="display: none;"><span class="fn"><?php the_author_posts_link(); ?></span></span>
-			<span class="updated" style="display:none;"><?php the_modified_time( 'c' ); ?></span>	
+			<span class="updated" style="display:none;"><?php the_modified_time( 'c' ); ?></span>		
 			<?php if( ! post_password_required($post->ID) ): ?>
 			<?php global $smof_data; if(!$smof_data['featured_images_pages'] ): ?>
 			<?php if($smof_data['legacy_posts_slideshow']):
@@ -130,14 +162,27 @@ get_header(); ?>
 				<?php wp_link_pages(); ?>
 			</div>
 			<?php if( ! post_password_required($post->ID) ): ?>
+			<?php if(class_exists('Woocommerce')): ?>
+			<?php if($smof_data['comments_pages'] && !is_cart() && !is_checkout() && !is_account_page() && !is_page(get_option('woocommerce_thanks_page_id'))): ?>
+				<?php
+				wp_reset_query();
+				comments_template();
+				?>
+			<?php endif; ?>
+			<?php else: ?>
 			<?php if($smof_data['comments_pages']): ?>
 				<?php
 				wp_reset_query();
 				comments_template();
 				?>
 			<?php endif; ?>
+			<?php endif; ?>
 			<?php endif; // password check ?>
 		</div>
-		<?php endwhile; ?>
+		<?php endif; ?>
 	</div>
+	<?php if( $sidebar_exists == true ): ?>
+	<?php wp_reset_query(); ?>
+	<div id="sidebar" style="<?php echo $sidebar_css; ?>"><?php generated_dynamic_sidebar(); ?></div>
+	<?php endif ;?>
 <?php get_footer(); ?>
