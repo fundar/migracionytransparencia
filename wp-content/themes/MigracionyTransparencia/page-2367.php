5777e55835cd7fe70130283e7120ae2bfbce48f9
CeciliaMadrid <lya.madrid@gmail.com>
2014-08-04 23:11:41 -0500

    buscador/*numeralia contenedores

--- wp-content/themes/MigracionyTransparencia/page-2367.php
+++ wp-content/themes/MigracionyTransparencia/page-2367.php
@@ -121,10 +121,38 @@ get_header(); ?>
 					<?php endif; ?>
 				</ul>
 			</div>
+			
 			<?php endif; ?>
 			<?php endif; ?>
 			<?php endif; ?>
 			<?php endif; // password check ?>
+			<div id="content" class="<?php echo $content_class; ?>" style="<?php echo $content_css; ?>">
+		<?php if($smof_data['blog_layout'] == 'Timeline'): ?>
+		<div class="timeline-icon"><i class="icon-bubbles"></i></div>
+		<?php endif; ?>
+		<div id="posts-container" class="<?php echo $container_class; ?> clearfix">
+			contenido
+		</div>
+		<?php themefusion_pagination($pages = '', $range = 2); ?>
+	</div>
+	<?php if( $sidebar_exists == true ): ?>
+	<?php wp_reset_query(); ?>
+	<div id="sidebar" style="<?php echo $sidebar_css; ?>">
+		<?php
+		if(is_home()) {
+			$name = get_post_meta(get_option('page_for_posts'), 'sbg_selected_sidebar_replacement', true);
+			if($name) {
+				generated_dynamic_sidebar($name[0]);
+			}
+		}
+		if(is_front_page()) {
+			if (!function_exists('dynamic_sidebar') || !dynamic_sidebar('SidebarHome')):
+			endif;
+		}
+		?>
+	</div>
+	<?php endif; ?>
+			
 			<div class="post-content">
 				<?php the_content(); ?>
 				<?php wp_link_pages(); ?>
