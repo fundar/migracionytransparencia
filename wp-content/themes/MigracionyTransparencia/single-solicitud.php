e94d202596049afeec3bc57760da84467ba8ba8e
CeciliaMadrid <lya.madrid@gmail.com>
2014-08-08 17:18:38 -0500

    Single solicitud

--- wp-content/themes/MigracionyTransparencia/single-solicitud.php
+++ wp-content/themes/MigracionyTransparencia/single-solicitud.php
@@ -34,8 +34,168 @@
 	}
 	?>
 	<div id="content" class="<?php echo $content_class; ?>" style="<?php echo $content_css; ?>">
+		<?php if(!$smof_data['blog_pn_nav']): ?>
+		<div class="single-navigation clearfix">
+			<?php previous_post_link('%link', __('Previous', 'Avada')); ?>
+			<?php next_post_link('%link', __('Next', 'Avada')); ?>
+		</div>
+		<?php endif; ?>
 		<?php if(have_posts()): the_post(); ?>
 		<div id="post-<?php the_ID(); ?>" <?php post_class('post'); ?>>
+			<?php
+			global $smof_data;
+			if( ! post_password_required($post->ID) ):
+			if((!$smof_data['legacy_posts_slideshow'] && !$smof_data['posts_slideshow']) && get_post_meta($post->ID, 'pyre_video', true)): ?>
+			<!--<div class="flexslider post-slideshow">
+				<ul class="slides">
+					<li class="full-video">
+						<?php echo get_post_meta($post->ID, 'pyre_video', true); ?>
+					</li>
+				</ul>
+			</div>-->
+			<?php endif;
+			if($smof_data['featured_images_single']):
+			if($smof_data['legacy_posts_slideshow']):
+			$args = array(
+			    'post_type' => 'attachment',
+			    'numberposts' => $smof_data['posts_slideshow_number']-1,
+			    'post_status' => null,
+			    'post_parent' => $post->ID,
+				'orderby' => 'menu_order',
+				'order' => 'ASC',
+				'post_mime_type' => 'image',
+				'exclude' => get_post_thumbnail_id()
+			);
+			$attachments = get_posts($args);
+			if((has_post_thumbnail() || get_post_meta($post->ID, 'pyre_video', true))):
+			?>
+			<div class="fusion-flexslider flexslider post-slideshow">
+				<ul class="slides">
+					<?php if(!$smof_data['posts_slideshow']): ?>
+					<?php if(get_post_meta($post->ID, 'pyre_video', true)): ?>
+					<li>
+						<div class="full-video">
+							<?php echo get_post_meta($post->ID, 'pyre_video', true); ?>
+						</div>
+					</li>
+					<?php elseif(has_post_thumbnail() ): ?>
+					<?php $attachment_image = wp_get_attachment_image_src(get_post_thumbnail_id(), 'full'); ?>
+					<?php $full_image = wp_get_attachment_image_src(get_post_thumbnail_id(), 'full'); ?>
+					<?php $attachment_data = wp_get_attachment_metadata(get_post_thumbnail_id()); ?>
+					<li>
+						<?php if( ! $smof_data['status_lightbox'] && ! $smof_data['status_lightbox_single'] ): ?>
+						<a href="<?php echo $full_image[0]; ?>" rel="prettyPhoto[gallery<?php the_ID(); ?>]" title="<?php echo get_post_field('post_excerpt', get_post_thumbnail_id()); ?>"><img src="<?php echo $attachment_image[0]; ?>" alt="<?php echo get_post_meta(get_post_thumbnail_id(), '_wp_attachment_image_alt', true); ?>" /></a>
+						<?php else: ?>
+						<img src="<?php echo $attachment_image[0]; ?>" alt="<?php echo get_post_meta(get_post_thumbnail_id(), '_wp_attachment_image_alt', true); ?>" />
+						<?php endif; ?>
+					</li>
+					<?php endif; ?>
+					<?php else: ?>
+					<?php if(get_post_meta($post->ID, 'pyre_video', true)): ?>
+					<li>
+						<div class="full-video">
+							<?php echo get_post_meta($post->ID, 'pyre_video', true); ?>
+						</div>
+					</li>
+					<?php endif; ?>
+					<?php if(has_post_thumbnail() && !get_post_meta($post->ID, 'pyre_video', true)): ?>
+					<?php $attachment_image = wp_get_attachment_image_src(get_post_thumbnail_id(), 'full'); ?>
+					<?php $full_image = wp_get_attachment_image_src(get_post_thumbnail_id(), 'full'); ?>
+					<?php $attachment_data = wp_get_attachment_metadata(get_post_thumbnail_id()); ?>
+					<li>
+						<?php if( ! $smof_data['status_lightbox'] && ! $smof_data['status_lightbox_single'] ): ?>
+						<a href="<?php echo $full_image[0]; ?>" rel="prettyPhoto[gallery<?php the_ID(); ?>]" title="<?php echo get_post_field('post_excerpt', get_post_thumbnail_id()); ?>"><img src="<?php echo $attachment_image[0]; ?>" alt="<?php echo get_post_meta(get_post_thumbnail_id(), '_wp_attachment_image_alt', true); ?>" /></a>
+						<?php else: ?>
+						<img src="<?php echo $attachment_image[0]; ?>" alt="<?php echo get_post_meta(get_post_thumbnail_id(), '_wp_attachment_image_alt', true); ?>" />
+						<?php endif; ?>
+					</li>
+					<?php endif; ?>
+					<?php foreach($attachments as $attachment): ?>
+					<?php $attachment_image = wp_get_attachment_image_src($attachment->ID, 'full'); ?>
+					<?php $full_image = wp_get_attachment_image_src($attachment->ID, 'full'); ?>
+					<?php $attachment_data = wp_get_attachment_metadata($attachment->ID); ?>
+					<li>
+						<?php if( ! $smof_data['status_lightbox'] && ! $smof_data['status_lightbox_single'] ): ?>
+						<a href="<?php echo $full_image[0]; ?>" rel="prettyPhoto[gallery<?php the_ID(); ?>]" title="<?php echo get_post_field('post_excerpt', $attachment->ID); ?>"><img src="<?php echo $attachment_image[0]; ?>" alt="<?php echo get_post_meta($attachment->ID, '_wp_attachment_image_alt', true); ?>" /></a>
+						<?php else: ?>
+						<img src="<?php echo $attachment_image[0]; ?>" alt="<?php echo get_post_meta(get_post_thumbnail_id(), '_wp_attachment_image_alt', true); ?>" />
+						<?php endif; ?>
+					</li>
+					<?php endforeach; ?>
+					<?php endif; ?>
+				</ul>
+			</div>
+			<?php endif; ?>
+			<?php else: ?>
+			<?php
+			if((has_post_thumbnail() || get_post_meta($post->ID, 'pyre_video', true))):
+			?>
+			<div class="fusion-flexslider flexslider post-slideshow">
+				<ul class="slides">
+					<?php if(!$smof_data['posts_slideshow']): ?>
+					<?php if(get_post_meta($post->ID, 'pyre_video', true)): ?>
+					<li>
+						<div class="full-video">
+							<?php echo get_post_meta($post->ID, 'pyre_video', true); ?>
+						</div>
+					</li>
+					<?php elseif(has_post_thumbnail() ): ?>
+					<?php $attachment_image = wp_get_attachment_image_src(get_post_thumbnail_id(), 'full'); ?>
+					<?php $full_image = wp_get_attachment_image_src(get_post_thumbnail_id(), 'full'); ?>
+					<?php $attachment_data = wp_get_attachment_metadata(get_post_thumbnail_id()); ?>
+					<li>
+						<?php if( ! $smof_data['status_lightbox'] && ! $smof_data['status_lightbox_single'] ): ?>
+						<a href="<?php echo $full_image[0]; ?>" rel="prettyPhoto[gallery<?php the_ID(); ?>]" title="<?php echo get_post_field('post_excerpt', get_post_thumbnail_id()); ?>"><img src="<?php echo $attachment_image[0]; ?>" alt="<?php echo get_post_meta(get_post_thumbnail_id(), '_wp_attachment_image_alt', true); ?>" /></a>
+						<?php else: ?>
+						<img src="<?php echo $attachment_image[0]; ?>" alt="<?php echo get_post_meta(get_post_thumbnail_id(), '_wp_attachment_image_alt', true); ?>" />
+						<?php endif; ?>
+					</li>
+					<?php endif; ?>
+					<?php else: ?>
+					<?php if(get_post_meta($post->ID, 'pyre_video', true)): ?>
+					<li>
+						<div class="full-video">
+							<?php echo get_post_meta($post->ID, 'pyre_video', true); ?>
+						</div>
+					</li>
+					<?php endif; ?>
+					<?php if(has_post_thumbnail() ): ?>
+					<?php $attachment_image = wp_get_attachment_image_src(get_post_thumbnail_id(), 'full'); ?>
+					<?php $full_image = wp_get_attachment_image_src(get_post_thumbnail_id(), 'full'); ?>
+					<?php $attachment_data = wp_get_attachment_metadata(get_post_thumbnail_id()); ?>
+					<li>
+						<?php if( ! $smof_data['status_lightbox'] && ! $smof_data['status_lightbox_single'] ): ?>
+						<a href="<?php echo $full_image[0]; ?>" rel="prettyPhoto[gallery<?php the_ID(); ?>]" title="<?php echo get_post_field('post_excerpt', get_post_thumbnail_id()); ?>"><img src="<?php echo $attachment_image[0]; ?>" alt="<?php echo get_post_meta(get_post_thumbnail_id(), '_wp_attachment_image_alt', true); ?>" /></a>
+						<?php else: ?>
+						<img src="<?php echo $attachment_image[0]; ?>" alt="<?php echo get_post_meta(get_post_thumbnail_id(), '_wp_attachment_image_alt', true); ?>" />
+						<?php endif; ?>
+					</li>
+					<?php endif; ?>
+					<?php
+					$i = 2;
+					while($i <= $smof_data['posts_slideshow_number']):
+					$attachment_new_id = kd_mfi_get_featured_image_id('featured-image-'.$i, 'post');
+					if($attachment_new_id):
+					?>
+					<?php $attachment_image = wp_get_attachment_image_src($attachment_new_id, 'full'); ?>
+					<?php $full_image = wp_get_attachment_image_src($attachment_new_id, 'full'); ?>
+					<?php $attachment_data = wp_get_attachment_metadata($attachment_new_id); ?>
+					<li>
+						<?php if( ! $smof_data['status_lightbox'] && ! $smof_data['status_lightbox_single'] ): ?>
+						<a href="<?php echo $full_image[0]; ?>" rel="prettyPhoto[gallery<?php the_ID(); ?>]" title="<?php echo get_post_field('post_excerpt', $attachment_new_id); ?>"><img src="<?php echo $attachment_image[0]; ?>" alt="<?php echo get_post_meta($attachment_new_id, '_wp_attachment_image_alt', true); ?>" /></a>
+						<?php else: ?>
+						<img src="<?php echo $attachment_image[0]; ?>" alt="<?php echo get_post_meta($attachment_new_id, '_wp_attachment_image_alt', true); ?>" />
+						<?php endif; ?>
+					</li>
+					<?php endif; $i++; endwhile; ?>
+					<?php endif; ?>
+				</ul>
+			</div>
+			<?php endif; ?>
+			<?php endif; ?>
+			<?php endif; ?>
+			<?php endif; ?>
+			<?php if($smof_data['blog_post_title']): ?>
 			<h2 class="entry-title"><?php the_title(); ?></h2>
 			<?php else: ?>
 			<span class="entry-title" style="display: none;"><?php the_title(); ?></span>
@@ -162,7 +322,6 @@
 	<?php if( $sidebar_exists == true ): ?>
 	<?php wp_reset_query(); ?>
 	<div id="sidebar" style="<?php echo $sidebar_css; ?>">
-	jioje9iojerw8ijtui4jhrtii4rhtuh45reuiitrh
 	<?php generated_dynamic_sidebar(); ?>
 	</div>
 	<?php endif; ?>
