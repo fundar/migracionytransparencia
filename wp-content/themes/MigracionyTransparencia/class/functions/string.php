d293f5e51a1d707eb404df8370fcc609c9de8d45
Carlos Hugo (Fundar) <caarloshugo@gmail.com>
2014-08-11 16:47:24 -0500

    clases y funciones para custom templates de la busqueda

---
+++ wp-content/themes/MigracionyTransparencia/class/functions/string.php
@@ -0,0 +1,35 @@
+<?php
+function getArray($text, $pos = NULL) {
+	$text = ltrim($text, "{");
+	$text = rtrim($text, "}");
+	$data = explode(",", $text);
+
+	if($pos !== NULL) {
+		return $data[$pos];
+	} else {
+		return $data;
+	}
+}
+
+function getTotalArray($text) {
+	$text = ltrim($text, "{");
+	$text = rtrim($text, "}");
+	$data = explode(",", $text);
+	
+	return count($data);
+}
+
+function getRoute($pos = false) {
+	
+	$route = explode("/", substr($_SERVER["REQUEST_URI"], 1));
+	
+	if($pos) {
+		if(isset($route[$pos])) {
+			return $route[$pos];
+		} else {
+			return false;
+		}
+	}
+	
+	return $route;
+}
---
+++ wp-content/themes/MigracionyTransparencia/class/search.php
@@ -0,0 +1,15 @@
+<?php
+
+include_once "db/db.php";
+
+class Search  {
+	
+	public function __construct() {
+		include_once "config/database.php";
+	}
+	
+	public function byName() {
+		return true;
+	}
+
+}
