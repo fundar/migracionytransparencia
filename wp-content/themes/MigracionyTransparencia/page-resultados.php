ca99ec1ddd121ba0cff7cc57afb573c5008f687c
Carlos Hugo (Fundar) <caarloshugo@gmail.com>
2014-08-13 18:38:14 -0500

    quitando busqueda

--- wp-content/themes/MigracionyTransparencia/page-resultados.php
+++ wp-content/themes/MigracionyTransparencia/page-resultados.php
@@ -1,7 +1,8 @@
 <?php get_header(); ?>
 <link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_directory'); ?>/css/buscar.css" />
 <div class="avada-row">
-<div id="content" class="full-width" style="width:100%">
+	<div id="content" class="full-width" style="width:100%">
+        <!--
         <div class="search-page-search-form">
 		<div class="busca">
 			<p class="la-busqueda"><span>Buscar</span></p>
@@ -13,27 +14,31 @@
 				<div class="search-button"><input type="submit" id="searchsubmit" value="&#xf002;" /></div>
 			</div>
 		</form>
+		-->
 	</div>
+	
 	<div id="posts-container" class=" clearfix">
 		<div class="post-content">
 			<div class="meta">
-                               <div class="date">
-                                <p class="year"><span>2013</span></p>
-                                <p class="month">11/02</p>
-                               </div>
-                       </div>
+				<div class="date">
+					<p class="year"><span>2013</span></p>
+					<p class="month">11/02</p>
+				</div>
+			</div>
 			<div class="excerpt-container strip-html">
-                                 <h2 class="entry-title"><a href="">asdasdasdasdasdsad</a></h2>	
-                                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eu posuere urna. Nullam sapien nulla, posuere ut lectus a, aliquet gravida justo. Phasellus et venenatis orci. Suspendisse vestibulum lorem leo, at euismod metus vehicula sed. Sed ornare semper facilisis... </p>
-                        </div>					
+				<h2 class="entry-title">
+					<a href="">Prueba</a>
+				</h2>
+				<p>Prueba</p>
+			</div>
+			
+			<div class="fusion-sharing-box share-box">					
+				<?php echo $social_icons->render_social_icons( $sharingbox_soical_icon_options ); ?>
+			</div>						
 		</div>
-	<div class="fusion-sharing-box share-box">					
-		<?php echo $social_icons->render_social_icons( $sharingbox_soical_icon_options ); ?>
-	</div>	
 	</div>
-	<div class="divisor-gris-dotted"></div>
-				
 	
+	<div class="divisor-gris-dotted"></div>
 </div>
 </div>
 
