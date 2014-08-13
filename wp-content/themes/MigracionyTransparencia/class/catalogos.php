8a410cb9a27055966de54a29754b1f63c95b9c71
Carlos Hugo (Fundar) <caarloshugo@gmail.com>
2014-08-13 15:59:01 -0500

    add catlogos class

---
+++ wp-content/themes/MigracionyTransparencia/class/catalogos.php
@@ -0,0 +1,56 @@
+<?php
+
+/*incluir clase para manejra la BD*/
+include_once "db/db.php";
+
+class Catalogos {
+	
+	public function __construct() {
+		/*configuración de base de datos*/
+		include_once "config/database.php";
+		
+		/*conexion con base de datos*/
+		$this->mysql = new Db();
+		$this->mysql->connect($db);
+	}
+	
+	/*categories*/
+	public function categories() {
+		$query = "select * from categories";
+		$data  = $this->mysql->query($query);
+		
+		return $data;
+	}
+	
+	/*dependencies*/
+	public function dependencies() {
+		$query = "select * from dependencies";
+		$data  = $this->mysql->query($query);
+		
+		return $data;
+	}
+	
+	/*organizations*/
+	public function organizations() {
+		$query = "select * from organizations";
+		$data  = $this->mysql->query($query);
+		
+		return $data;
+	}
+	
+	/*answersTypes*/
+	public function answersTypes() {
+		$query = "select * from answers_types";
+		$data  = $this->mysql->query($query);
+		
+		return $data;
+	}
+	
+	/*Select distinct years for requests*/
+	public function years() {
+		$query = "select distinct(year(date_published)) as year from requests order by year desc";
+		$data  = $this->mysql->query($query);
+		
+		return $data;
+	}
+}
