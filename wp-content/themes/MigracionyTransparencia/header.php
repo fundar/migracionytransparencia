3bebb1a055bd63527c49decc7c18041eac239603
Carlos Hugo (Fundar) <caarloshugo@gmail.com>
2014-08-13 13:13:56 -0500

    utf8

--- wp-content/themes/MigracionyTransparencia/header.php
+++ wp-content/themes/MigracionyTransparencia/header.php
@@ -1,8 +1,6 @@
 <!DOCTYPE html>
-<html xmlns="http<?php echo (is_ssl())? 's' : ''; ?>://www.w3.org/1999/xhtml" <?php language_attributes(); ?>>
+<html>
 <head>
-	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
-
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
 
 	<title>
