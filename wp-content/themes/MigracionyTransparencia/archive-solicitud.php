b2676228353e9ad398ac8e1368ec261535ccdb3e
Carlos Hugo (Fundar) <caarloshugo@gmail.com>
2014-08-14 15:55:04 -0500

    cerrando div

--- wp-content/themes/MigracionyTransparencia/archive-solicitud.php
+++ wp-content/themes/MigracionyTransparencia/archive-solicitud.php
@@ -28,60 +28,59 @@
 	<div id="content" class="full-width" style="width:100%">
         <!--
         <div class="search-page-search-form">
-		<div class="busca">
-			<p class="la-busqueda"><span>Buscar</span></p>
+			<div class="busca">
+				<p class="la-busqueda"><span>Buscar</span></p>
+			</div>
+		
+			<form id="searchform" class="seach-form" role="search" method="get" action="<?php echo home_url( '/' ); ?>">
+				<div class="search-table">
+					<div class="search-field"><input type="text" value="" name="s" id="s" placeholder="<?php _e( 'Search ...', 'Avada' ); ?>"/></div>
+					<div class="search-button"><input type="submit" id="searchsubmit" value="&#xf002;" /></div>
+				</div>
+			</form>
+		</div>
+		-->
+	
+		<div id="posts-container" class=" clearfix">
+			<?php foreach($requests as $request) { ?>
+				<div class="post-content">
+					<div class="meta">
+						<div class="date">
+							<p class="year">
+								<span>
+									<?php echo getYear($request["date_published"]);?>
+								</span>
+							</p>
+							<p class="month">
+								<span>
+									<?php echo getMonth($request["date_published"]);?>
+								</span>
+							</p>
+						</div>
+					</div>
+					<div class="excerpt-container strip-html">
+						<h2 class="entry-title">
+							<a href="/solicitudes/solicitud/?slug=<?php echo $request["slug"];?>" title="<?php echo utf8_encode($request["short_name"]);?>">
+								<?php echo utf8_encode($request["short_name"]);?>
+							</a>
+						</h2>
+						<p><?php echo utf8_encode($request["question"]);?></p>
+					</div>
+					
+					<div class="fusion-sharing-box share-box">					
+						<?php echo $social_icons->render_social_icons( $sharingbox_soical_icon_options ); ?>
+					</div>						
+				</div>
+				<div class="divisor-gris-dotted"></div>
+			<?php } ?>
 		</div>
 		
-		<form id="searchform" class="seach-form" role="search" method="get" action="<?php echo home_url( '/' ); ?>">
-			<div class="search-table">
-				<div class="search-field"><input type="text" value="" name="s" id="s" placeholder="<?php _e( 'Search ...', 'Avada' ); ?>"/></div>
-				<div class="search-button"><input type="submit" id="searchsubmit" value="&#xf002;" /></div>
-			</div>
-		</form>
-		-->
-	</div>
-	
-	<div id="posts-container" class=" clearfix">
-		<?php foreach($requests as $request) { ?>
-			<div class="post-content">
-				<div class="meta">
-					<div class="date">
-						<p class="year">
-							<span>
-								<?php echo getYear($request["date_published"]);?>
-							</span>
-						</p>
-						<p class="month">
-							<span>
-								<?php echo getMonth($request["date_published"]);?>
-							</span>
-						</p>
-					</div>
-				</div>
-				<div class="excerpt-container strip-html">
-					<h2 class="entry-title">
-						<a href="/solicitudes/solicitud/?slug=<?php echo $request["slug"];?>" title="<?php echo utf8_encode($request["short_name"]);?>">
-							<?php echo utf8_encode($request["short_name"]);?>
-						</a>
-					</h2>
-					<p><?php echo utf8_encode($request["question"]);?></p>
-				</div>
-				
-				<div class="fusion-sharing-box share-box">					
-					<?php echo $social_icons->render_social_icons( $sharingbox_soical_icon_options ); ?>
-				</div>						
-			</div>
-			<div class="divisor-gris-dotted"></div>
+		<?php for($page=1; $page<=$pages; $page++) { ?>
+			<a href="/solicitudes/?page=<?php echo $page; ?>" title="Paginación de solicitudes"<?php echo ($currentPage==$page) ? ' class="active-page"' : '';?>>
+				<?php echo $page; ?>
+			</a>
 		<?php } ?>
 	</div>
-	
-	<?php for($page=1; $page<=$pages; $page++) { ?>
-		<a href="/solicitudes/?page=<?php echo $page; ?>" title="Paginación de solicitudes"<?php echo ($currentPage==$page) ? ' class="active-page"' : '';?>>
-			<?php echo $page; ?>
-		</a>
-	<?php } ?>
-	
-</div>
 </div>
 
 <?php get_footer(); ?>
