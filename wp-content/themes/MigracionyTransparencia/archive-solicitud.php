5f6cd924eaca52ca17738367563c50220f6ed7fd
Carlos Hugo (Fundar) <caarloshugo@gmail.com>
2014-08-15 12:22:36 -0500

    active page

--- wp-content/themes/MigracionyTransparencia/archive-solicitud.php
+++ wp-content/themes/MigracionyTransparencia/archive-solicitud.php
@@ -101,7 +101,7 @@
 		
 		<?php if(isset($pages)) { ?>
 			<?php for($page=1; $page<=$pages; $page++) { ?>
-				<a href="/solicitudes/?page=<?php echo $page; ?>" title="Paginación de solicitudes"<?php echo ($currentPage==$page) ? ' class="current-pag pag"' : ' class="pag"';?>>
+				<a href="/solicitudes/?page=<?php echo $page; ?>" title="Paginación de solicitudes"<?php echo ($currentPage==$page) ? ' class="active-page pag"' : ' class="pag"';?>>
 					<?php echo $page; ?>
 				</a>
 			<?php } ?>
