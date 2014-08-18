49d9d20c641e3b98c852dfcd69e6cac19bc4ae7b
Carlos Hugo (Fundar) <caarloshugo@gmail.com>
2014-08-18 17:59:56 -0500

    debug query . debug false

--- wp-content/themes/MigracionyTransparencia/class/search.php
+++ wp-content/themes/MigracionyTransparencia/class/search.php
@@ -26,7 +26,6 @@ class Search {
 	/*busca las solicitudes por query de busqueda*/
 	public function byQuery($queryString) {
 		$query = "select requests.*, organizations.name as organization, dependencies.name as dependecy, categories.name as category from requests left join organizations on requests.id_organization=organizations.id_organization left join dependencies on requests.id_dependecy=dependencies.id_dependecy left join categories on categories.id_category=requests.id_category " . $queryString . " order by requests.date_published desc";
-		die(var_dump($query));
 		$data  = $this->mysql->query($query);
 		
 		if($data and is_array($data)) return $data;
