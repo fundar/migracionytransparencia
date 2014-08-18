8c1a6d4767489c55f5f50b856643d4a239e118b4
CeciliaMadrid <lya.madrid@gmail.com>
2014-08-18 18:19:16 -0500

    estructura búsqueda

--- wp-content/themes/MigracionyTransparencia/archive-solicitud.php
+++ wp-content/themes/MigracionyTransparencia/archive-solicitud.php
@@ -44,26 +44,24 @@
 	<div id="content" class="full-width" style="width:100%">
                 <div class="search-page-search-form">
 			<div class="fusion-three-fourth three_fourth fusion-column">
-			<a class="link" href="/#busqueda" title="Regrear  eda">
-				Regresar al formulario de b&uacute;squeda
-			</a>
-			</div>
-			<div class="criterios">
-				<div class="fusion-one-fourth one_fourth fusion-column last">
 				<p class="resultados">
-					<span>Resultado de la b&uacute;squeda</span>
+				<span>Rsultado de la b&uacute;squeda</span>
 					<span class="total">&nbsp;-&nbsp;<?php echo $count;?> solicitudes encontradas</span>
-				</p>
-				</div>
-				
+				</p>			
 				<p class="criterios-busqueda">
 					Criterios de b&uacute;squeda:<br/>
 					<?php echo $criterios;?>
 				</p>
+			</div>
+			</div>
+			<div class="criterios">
+				<div class="fusion-one-fourth one_fourth fusion-column last">
+ 			                 <a class="link" href="/#busqueda" title="Regrear  eda">
+				         Regresar al formulario de b&uacute;squeda
+			                 </a>
 				</div>
 			</div>
-		</div>
-		
+	           
 		<div id="posts-container" class=" clearfix">
 			<?php foreach($requests as $request) { ?>
 				<div class="post-content">
